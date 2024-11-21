1f6b829d93e790b48b29158d53ae4e30020a2dfc
Samuel <samuelajala01@gmail.com>
2024-11-22 00:07:46 +0100

    feat: add new pages

---
+++ src/app/components/PatientHomeBtn.jsx
@@ -0,0 +1,8 @@
+const PatientHomeBtn = ({title}) => {
+    return ( <>
+    <button className="bg-[var(--pry)]">
+        <p className="px-4 py-8 text-white text-2xl">{title}</p>
+        </button></> );
+}
+ 
+export default PatientHomeBtn;
